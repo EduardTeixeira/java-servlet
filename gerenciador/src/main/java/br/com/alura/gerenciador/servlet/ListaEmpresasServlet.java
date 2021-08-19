59a181156f5beb9936a11c5d8f60d4cbe913ce87
unknown <eduard.teixeira@etec.sp.gov.br>
2021-08-19 10:29:53 -0300

    paginas dinamicas com jsp

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,16 +22,10 @@ public class ListaEmpresasServlet extends HttpServlet {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<ul>");
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		out.println("</ul>");
-		out.println("</body>");
-		out.println("</html>");
+		request.setAttribute("empresas", lista);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);
 
 	}
 
--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,8 +1,8 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,16 +23,14 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
-		
+
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Empresa " + nomeEmpresa + " cadastrada!");
-		out.println("</body>");
-		out.println("</html>");
+		// chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 
 	}
 
